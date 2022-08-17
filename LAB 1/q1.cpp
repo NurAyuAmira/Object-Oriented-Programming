0afe7c3ffab79a270f8d13798fb1f760a82a33a4
yuz7_ <94117067+Yuz9690@users.noreply.github.com>
2022-08-18 00:57:48 +0800

    Create q1.cpp

---
+++ LAB 1/q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	cout << "MMU Fee Structure 2021" << endl
+		<<"Programme	Duration	Fee "<<endl
+		<<"Diploma		2 Years		23000"<< endl
+		<<"Degree		3 Years		60000";
+	
+}
